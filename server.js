bac77a2bc70bdf851d1a7d62fa4f8c74709eb925
chris6661 <74381468+chris6661@users.noreply.github.com>
2022-05-10 22:48:40 -0500

    added function to find every third letter from string

--- server.js
+++ server.js
@@ -6,3 +6,21 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(__dirname + '/public'));
 
+//get request for the home page
+app.get('/', (req, res) => {
+    res.send("index.html"); 
+}); //end of get request for the home page
+
+// need method to return json data with key return_string, every third letter
+
+// input type would be a string entered from html form
+// string has to be at least 3 letters long, make sure to add conditionals
+
+function cut_str(str) {
+    let return_string = "";
+    // start at third character, add 3 to find next char to add to return_string
+    for (let i = 2; i < str.length; i += 3) {
+        return_string += str[i]; 
+    }
+    return return_string;
+}
