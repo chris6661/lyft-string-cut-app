a9b548853f9cf7268f125e8a332bf3d831ce9abd
chris6661 <74381468+chris6661@users.noreply.github.com>
2022-05-11 00:03:22 -0500

    strings are being cut and added to html

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ app.use(express.static(__dirname + '/public'));
 //get request for the home page
 app.get('/', (req, res) => {
     res.send("index.html"); 
-}); //end of get request for the home page
+}); 
+//end of get request for the home page
 
 // need method to return json data with key return_string, every third letter
 
@@ -24,3 +25,34 @@ function cut_str(str) {
     }
     return return_string;
 }
+
+// post request for home page, accepts argument "string_to_cut" from html form
+app.post("/test", (req, res) => {
+    const {string_to_cut } = req.body; 
+
+    if (string_to_cut.length >= 3) {
+        res.send(`
+        <html>
+            <head>
+                <link rel = "stylesheet" href = "style.css">
+            </head>
+            <body class = "result">
+                <h2>Result: </h2>
+                <h2>{"return_string": ${JSON.stringify(cut_str(string_to_cut))} }</h2>
+                <a href = "/"><button>Go Back</button></a>
+                </body>
+                </html>
+                `
+        ); 
+    } else if (!string_to_cut) {
+        res.status(400).send("There is no 'string_to_cut' key in the request body.");
+    } else {
+        res.status(400).send("Please enter a string with at least 3 letters.");
+    }
+});
+ //end of post request for home page
+
+ const PORT = process.env.PORT || 3000;
+ app.listen(PORT, () => {
+     console.log(`Server is running on port ${PORT}`);
+ })
